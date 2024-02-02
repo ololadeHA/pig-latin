fce3038118efa94674e459051d25315d9a0f993f
Arowolo Haleem <arowolohaleem@gmail.com>
2024-02-02 10:10:10 +0100

    add js and css file to page

---
+++ js/scripts.js
@@ -0,0 +1,117 @@
+// // UI Logic
+
+// $(document).ready(function () {
+//   $("form#formWord").submit(function (event) {
+//       event.preventDefault();
+
+//       const passage = $("#sentence").val();
+
+//       let result = pigLatin(passage);
+//       $("#result").text(result);
+
+
+//   });
+// });
+
+
+ 
+// // Business Logic
+// function pigLatin(passage) {
+//   if (passage.trim().length === 0) {
+//       return '';
+//   }
+
+//   let vowels = "aeiou";
+//   let returnString = "";
+//   const wordArray = passage.split(" ");
+//   console.log(wordArray);
+
+//   for (let i = 0; i < wordArray.length; i++) {
+//       let currentWord = wordArray[i];
+//       if (wordArray[i] !== "") {
+//           if (vowels.includes(currentWord.charAt(0).toLowerCase())) {
+//               wordArray[i] = currentWord.concat("way");
+//           } else {
+//               wordArray[i] = modifiedConsonant(currentWord);
+//           }
+//       }
+//   }
+//   returnString = wordArray.join(" ");
+//   return returnString.trim();
+// }
+
+// function modifiedConsonant(word) {
+//   let vowels = "aeiou";
+//   let characterWords = word.split("");
+//   let returnString = "";
+
+//   for (let i = 0; i < characterWords.length; i++) {
+//       if ("q" === characterWords[i].charAt(0).toLowerCase()) {
+//           returnString = word.slice(2).concat(characterWords[i] + characterWords[i + 1] + "ay");
+//           return returnString;
+//       } else if (vowels.includes(characterWords[i].toLowerCase())) {
+//           returnString = word.slice(i).concat(word.slice(0, i) + "ay");
+//           return returnString;
+//       }
+//   }
+
+//   return word;
+// }// UI Logic
+
+// $(document).ready(function () {
+//   $("form#word-merch").submit(function (event) {
+//       event.preventDefault();
+
+//       const passage = $("#text-passage").val();
+
+//       let result = pigLatin(passage);
+//       $("#result").text(result);
+
+
+//   });
+// });
+
+
+
+// // Business Logic
+// function pigLatin(passage) {
+//   if (passage.trim().length === 0) {
+//       return '';
+//   }
+
+//   let vowels = "aeiou";
+//   let returnString = "";
+//   const wordArray = passage.split(" ");
+//   console.log(wordArray);
+
+//   for (let i = 0; i < wordArray.length; i++) {
+//       let currentWord = wordArray[i];
+//       if (wordArray[i] !== "") {
+//           if (vowels.includes(currentWord.charAt(0).toLowerCase())) {
+//               wordArray[i] = currentWord.concat("way");
+//           } else {
+//               wordArray[i] = modifiedConsonant(currentWord);
+//           }
+//       }
+//   }
+//   returnString = wordArray.join(" ");
+//   return returnString.trim();
+// }
+
+// function modifiedConsonant(word) {
+//   let vowels = "aeiou";
+//   let characterWords = word.split("");
+//   let returnString = "";
+
+//   for (let i = 0; i < characterWords.length; i++) {
+//       if ("q" === characterWords[i].charAt(0).toLowerCase()) {
+//           returnString = word.slice(2).concat(characterWords[i] + characterWords[i + 1] + "ay");
+//           return returnString;
+//       } else if (vowels.includes(characterWords[i].toLowerCase())) {
+//           returnString = word.slice(i).concat(word.slice(0, i) + "ay");
+//           return returnString;
+//       }
+//   }
+
+//   return word;
+// }
---
+++ readme.md
@@ -0,0 +1,30 @@
+<!-- >## Project Owner; Arowolo Haleem
+
+>## PIG-LATIN
+* This page gives an overview of my Pig Latin Website
+
+>## Technologies used
+
+* HTML
+* CSS
+* BOOTSTRAP
+* JQUERY
+
+>## Description
+
+Welcome to my project. This project allow users to input text and returns it back in another language know as Pig Latin
+
+>## Setup/Installation requirements
+* Clone this repository to your desktop
+* Navigate to the top level of your directory
+* Click on Pig-Latin/index.html
+
+>## Known Bugs
+* NONE
+
+>## License
+* MIT
+
+>## Contact information
+* Phone: 09031785226
+* Email: arowolohaleem@gmail.com -->
